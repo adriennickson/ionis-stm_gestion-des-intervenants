85f617fa6907e8a6a1a6134d71f7be77692250fb
MOMO Adrien Nickson <mani@ekwali.local>
2021-02-22 12:54:19 +0100

    premier commit

---
+++ src/main/java/com/ionisstm/intervenants/IntervenantsApplication.java
@@ -0,0 +1,13 @@
+package com.ionisstm.intervenants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervenantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntervenantsApplication.class, args);
+    }
+
+}
