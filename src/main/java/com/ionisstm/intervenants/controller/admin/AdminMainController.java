d5e939c8e9dc65bad4a2c97a60528958d4f04fd4
MOMO Adrien Nickson <mani@ekwali.local>
2021-02-25 22:32:15 +0100

    Recherche validégit add .!

--- src/main/java/com/ionisstm/intervenants/controller/admin/AdminMainController.java
+++ src/main/java/com/ionisstm/intervenants/controller/admin/AdminMainController.java
@@ -1,6 +1,8 @@
 package com.ionisstm.intervenants.controller.admin;
 
+import com.ionisstm.intervenants.model.Speaker;
 import com.ionisstm.intervenants.model.User;
+import com.ionisstm.intervenants.repository.SpeakerRepository;
 import com.ionisstm.intervenants.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -8,13 +10,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class AdminMainController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
 
     @RequestMapping(value="/admin/home", method = RequestMethod.GET)
     public ModelAndView home(){
@@ -23,6 +31,16 @@ public class AdminMainController {
         User user = userService.findUserByUserName(auth.getName());
         modelAndView.addObject("user", user);
         modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/admin/search", method = RequestMethod.GET)
+    public ModelAndView search(@RequestParam(required = false) String q){
+        System.out.println(q);
+        ModelAndView modelAndView = new ModelAndView();
+        List<Speaker> speakers = speakerRepository.FindAllWithJoinSubjectByName( "%"+q+"%");
+        modelAndView.addObject("speakers", speakers);
+        modelAndView.setViewName("admin/search");
         return modelAndView;
     }
 
--- src/main/java/com/ionisstm/intervenants/repository/SpeakerRepository.java
+++ src/main/java/com/ionisstm/intervenants/repository/SpeakerRepository.java
@@ -2,9 +2,17 @@ package com.ionisstm.intervenants.repository;
 
 import com.ionisstm.intervenants.model.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface SpeakerRepository extends JpaRepository<Speaker, Integer> {
+    Set<Speaker> findAllByNameContainingOrLastNameContaining(String a, String b);
 
+    @Query(value = "SELECT DISTINCT * FROM speakers JOIN speaker_subject ON speaker_subject.speaker_id=speakers.speaker_id "
+            +"JOIN subjects ON speaker_subject.supject_id=subjects.subject_id WHERE subjects.name LIKE :query", nativeQuery = true)
+    public List<Speaker> FindAllWithJoinSubjectByName(String query);
 }
